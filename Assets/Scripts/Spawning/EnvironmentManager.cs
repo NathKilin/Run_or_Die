3096fee1350b5495ad48c15312e77507b5a56f06
S3raphimm <167085675+S3raphimm@users.noreply.github.com>
2025-10-14 20:38:58 +0300

    bleh

--- Assets/Scripts/Spawning/EnvironmentManager.cs
+++ Assets/Scripts/Spawning/EnvironmentManager.cs
@@ -10,7 +10,7 @@ public class EnvironmentManager : MonoBehaviour, IObjectPooler
     private List<GameObject> objectPool;
     private List<GameObject> spawnedObjects;
     public GameObject[] environmentPrefabs;
-    public int countPerObject = 4;
+    public int countPerObject = 7;
 
     public GameObject player;
     
@@ -24,7 +24,7 @@ public class EnvironmentManager : MonoBehaviour, IObjectPooler
 
     private void GenerateInitialEnvironment()
     {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < countPerObject; i++) {
             SpawnNewEnvironmentPiece();
         }
     }
@@ -44,14 +44,18 @@ public class EnvironmentManager : MonoBehaviour, IObjectPooler
         Vector3 positionToSpawn = Vector3.zero;
 
         //      2.1. Get Piece Size
+        obj.SetActive(true); // Temporarily Enable EnvPiece To Calculate Bounds 
         EnvironmentPiece environmentPiece = obj.GetComponent<EnvironmentPiece>();
         Bounds pieceBounds = environmentPiece.GetWorldBounds();
+        obj.SetActive(false);
         
         //      2.2. Figure out where to spawn piece
         bool isOtherObject = spawnedObjects.Count != 0;
         if (isOtherObject) {
+            obj.SetActive(true);
             EnvironmentPiece topPiece = spawnedObjects[spawnedObjects.Count - 1].GetComponent<EnvironmentPiece>(); 
             Bounds topPieceBounds = topPiece.GetWorldBounds();
+            obj.SetActive(false);
             positionToSpawn = topPiece.transform.position;
             positionToSpawn.z = topPieceBounds.max.z + pieceBounds.extents.z;
         } else {
