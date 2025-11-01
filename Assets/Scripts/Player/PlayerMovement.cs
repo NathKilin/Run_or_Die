e74489afb5c808ecc302c61e73a972ad440c2df9
S3raphimm <167085675+S3raphimm@users.noreply.github.com>
2025-11-01 15:47:41 +0200

    Player Movement & Input Manager ( start )

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [Header("Movement Values")]
+    public float horizontalSpeed = 5.0f;
+    public float jumpForce = 7.0f;
+    public Vector3 currentDirection = Vector3.right;
+    
+    private InputHandler inputHandler;
+    private Rigidbody rigidBody;
+    
+    
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        inputHandler = FindObjectOfType<InputHandler>();
+        inputHandler.OnScreenTapped += Jump;
+    }
+
+    
+    void HandleHorizontalMovement()
+    {
+        rigidBody.linearVelocity = new Vector3(
+            currentDirection.x * horizontalSpeed,
+            rigidBody.linearVelocity.y,
+            currentDirection.z * horizontalSpeed);
+    }
+    
+
+    void Update(){
+        // Creates a constant jump force independent on speed
+        rigidBody.linearVelocity = new Vector3(
+            rigidBody.linearVelocity.x,
+            jumpForce,
+            rigidBody.linearVelocity.z);
+    }
+    
+
+    void FlipPlayer()
+    {
+        transform.Rotate(Vector3.up, 180);
+        currentDirection = currentDirection ==  Vector3.right ? Vector3.left : Vector3.right;
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Level"))
+            FlipPlayer();
+    }
+}
